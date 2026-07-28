dc54af206cc74e5407c5f2cc9364bb6aa76ec5e5
Shailesh Yadav <shailesh772002@gmail.com>
2026-07-28 11:20:47 +0530

    import module in rough.py file and then run

--- rough.py
+++ rough.py
@@ -1,7 +1,11 @@
-lst=[1,2,3]
-str="mlops playlist"
-my_int=1
+# lst=[1,2,3]
+# str="mlops playlist"
+# my_int=1
 
-print(type(lst))
-print(type(str))
-print(type(my_int))
+# print(type(lst))
+# print(type(str))
+# print(type(my_int))
+
+from oops_proj import ChatBook
+
+obj=ChatBook()
